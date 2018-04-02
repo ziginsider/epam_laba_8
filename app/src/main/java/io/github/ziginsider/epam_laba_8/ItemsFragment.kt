9080a6edbf05fa79b84c8a8d0712d30d29e4cb19
ziginsider <ziginsider@gmail.com>
2018-04-02 15:12:59 +0300

    add ItemsFragment

---
+++ app/src/main/java/io/github/ziginsider/epam_laba_8/ItemsFragment.kt
@@ -0,0 +1,8 @@
+package io.github.ziginsider.epam_laba_8
+
+class ItemsFragment : BaseFragment() {
+    override val logTag = ItemsFragment::class.java.simpleName;
+    override fun getLayout(): Int {
+        return R.layout.fragment_items
+    }
+}
