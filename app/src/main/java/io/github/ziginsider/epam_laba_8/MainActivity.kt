b6d911b1d5b698318a881d417872ceb4d044bc2a
ziginsider <ziginsider@gmail.com>
2018-04-02 22:09:58 +0300

    add MainActivity::setFragment()

--- app/src/main/java/io/github/ziginsider/epam_laba_8/MainActivity.kt
+++ app/src/main/java/io/github/ziginsider/epam_laba_8/MainActivity.kt
@@ -2,16 +2,21 @@ package io.github.ziginsider.epam_laba_8
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.app.Fragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val fragment = ItemsFragment()
+        setFragment(ItemsFragment())
+
+    }
+
+    private fun setFragment(fragment: Fragment) {
         supportFragmentManager
                 .beginTransaction()
-                .add(R.id.activity_layout, fragment)
+                .replace(R.id.activity_layout, fragment)
                 .commit()
     }
 }
